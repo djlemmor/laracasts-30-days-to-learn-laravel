9fc71ff351d2445d1022f74d16d926c24a280e12
DJ Lemmor Nuique <lemmornuique@gmail.com>
2025-08-23 04:04:45 +0800

    Ep 18 - Editing, Updating, and Deleting a Resource

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/about', function () {
     return view('about');
 });
 
+// Index
 Route::get('/jobs', function ()  {
     $jobs= Job::with(   'employer')->latest()->paginate(6);
 
@@ -30,16 +31,19 @@ Route::get('/jobs', function ()  {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id)  {
     $job = Job::find($id);
 
     return view('jobs.show',  ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs  ', function () {
     request()->validate([
         'title' => ['required', 'min: 5'],
@@ -52,6 +56,47 @@ Route::post('/jobs  ', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id)  {
+    $job = Job::find($id);
+
+    return view('jobs.edit',  ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id)  {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min: 5'],
+        'salary' => ['required'],
+    ]);
+
+    // authorizate (on hold)
+
+    // update the job and persist
+    $job = Job::findOrFail($id);
+    
+    $job->update([
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+    ]);
+
+    // redirect
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id)  {
+    // authorizate (on hold)
+    
+    // delete the job
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
