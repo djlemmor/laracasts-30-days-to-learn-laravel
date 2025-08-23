b2834d96788ed06335469a94df57305079751dc4
DJ Lemmor Nuique <lemmornuique@gmail.com>
2025-08-24 04:32:40 +0800

    Ep 21 - Make a Login and Registration System From Scratch: Part 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,29 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Login
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/login">
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
-                <p class="mt-1 text-sm/6 text-gray-600">We just need a handful details from you.</p>
-
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <x-form-field>
-                        <x-form-label for="title">Title</x-form-label>
+                        <x-form-label for="email">Email Address</x-form-label>
 
                         <div class="mt-2">
-                            <x-form-input id="title" name="title" placeholder="Pro Gamer" required />
-                            <x-form-error name="title" />
+                            <x-form-input id="email" name="email" placeholder="JohnDoe@gmail.com" type="email"
+                                required />
+                            <x-form-error name="email" />
                         </div>
                     </x-form-field>
 
                     <x-form-field>
-                        <x-form-label for="salary">Salary</x-form-label>
+                        <x-form-label for="password">Passwword</x-form-label>
 
                         <div class="mt-2">
-                            <x-form-input id="salary" name="salary" placeholder="$50,000 per year" required />
-                            <x-form-error name="salary" />
+                            <x-form-input id="password" name="password" type="password" required />
+                            <x-form-error name="password" />
                         </div>
                     </x-form-field>
                 </div>
@@ -43,8 +41,8 @@
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <x-form-button>Save</x-form-button>
+            <a href="/" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+            <x-form-button>Login</x-form-button>
         </div>
     </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,3 +40,10 @@ Route::controller(JobController::class)->group(function(){
 }); */
 
 Route::resource('jobs', JobController::class); 
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'login']);
