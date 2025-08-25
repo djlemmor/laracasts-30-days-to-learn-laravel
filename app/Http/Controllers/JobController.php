0be17c7414b336a3a5bb06bdf949b821a4a3ed72
DJ Lemmor Nuique <lemmornuique@gmail.com>
2025-08-26 04:40:09 +0800

    Ep 23 - 6 Steps to Authorization Mastery

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
